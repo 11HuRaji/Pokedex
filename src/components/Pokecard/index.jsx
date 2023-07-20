63bf2b4f75e611b996cf84a56495f0e696633c71
Hussein Raji <111huraji@gmail.com>
2023-07-20 13:00:07 +0100

    add remove button

--- src/components/Pokecard/index.jsx
+++ src/components/Pokecard/index.jsx
@@ -1,10 +1,23 @@
+import { usePokedex } from "../../contexts/usePokedex";
+
 function Pokecard({ name, image, type }) {
+  const { setPokeData } = usePokedex();
+
   return (
     <>
       <div className="pokecard" role="figure">
         <h2 className="pokemon-name">{name}</h2>
         <img src={image} alt={`An image of ${name}`} />
         <div className="pokemon-type">Type: {type}</div>
+        <button
+          onClick={() =>
+            setPokeData((state) =>
+              state.filter((pokemon) => pokemon.name !== name)
+            )
+          }
+        >
+          Remove
+        </button>
       </div>
     </>
   );
